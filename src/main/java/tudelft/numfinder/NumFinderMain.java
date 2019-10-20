2434097fffa59c8fe1eef7a9c54b0825ca2a691c
Rehab <rehab21@gmail.com>
2019-10-20 20:39:48 +0200

    complete the test class for addFive

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -1,15 +1,17 @@
 package tudelft.numfinder;
 
+import org.mockito.internal.matchers.Null;
+
 public class NumFinderMain {
 
     public static void main (String[] args) {
         NumFinder nf = new NumFinder();
 
         // this works
-        // nf.find(new int[] {4, 25, 7, 9});
+//         nf.find(new int[] {4, 25, 7, 9});
 
         // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
+        nf.find(new int[] {0, 3, -7, 1});
 
         System.out.println(nf.getLargest());
         System.out.println(nf.getSmallest());
